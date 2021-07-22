b6a56e98999ccc22e25c7026b62e896b5335de97
kambiz fakhr <83270531+kambizfakhr@users.noreply.github.com>
2021-07-22 23:25:58 +0430

    Add files via upload

---
+++ Home.js
@@ -0,0 +1,28 @@
+import { useState, useEffect } from "react";
+import BlogList from "./BlogList";
+const Home = () => {
+   const[blogs, setBlogs] = useState([
+       {title:'my new website', body:'lorem ispum...', author:'kambiz', id:1},
+       {title:'kafa academy', body:'lorem ispum...', author:'kafa', id:2},
+       {title:'React Learning', body:'lorem ispum...', author:'kambiz', id:3}
+   ]);
+   const handleDelete = (id) => {
+     const newBlog = blogs.filter(blog => blog.id!== id);
+     setBlogs(newBlog);
+   }
+    
+   useEffect(()=>{
+    console.log('use effect is running...');
+    console.log(blogs);
+   });
+    
+    return (
+        <div className="home">
+          <BlogList blogs={ blogs } title="salam" handleDelete={ handleDelete }/>
+          <BlogList blogs={ blogs.filter((blog) => blog.author==='kambiz') } title="مطالب کامبیز فخر"/>
+        </div>
+        
+     );
+}
+ 
+export default Home;
---
+++ Navbar.js
@@ -0,0 +1,13 @@
+const Navbar = () => {
+    return ( 
+        <nav className="navbar">
+            <h1>The KaFa Blog</h1>
+            <div className="link">
+                <a href="/">Home</a>
+                <a href="/create">New Blog</a>
+            </div>
+        </nav>
+     );
+}
+ 
+export default Navbar;
