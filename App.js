5143c42f8cef9ce980e3c2c7bd60deb9e1eb4c80
kambiz fakhr <83270531+kambizfakhr@users.noreply.github.com>
2021-09-25 17:33:15 +0330

    Add files via upload

---
+++ App.js
@@ -0,0 +1,33 @@
+import Navbar from './Navbar'
+import Home from './Home'
+import {BrowserRouter as Router , Route, Switch} from 'react-router-dom'
+import Create from './Create';
+import BlogDetails from './BlogDetails';
+function App() {
+  // const title = "welcome to my blog!";
+  // const likes = 50;
+  // const link = "http://www.wikichejoor.ir";
+  return (
+    <Router>
+      <div className="App">
+        <Navbar />
+      <div className="content">
+        <Switch>
+          <Route exact path="/">
+             <Home />
+          </Route>
+          <Route path="/create">
+             <Create />
+          </Route>
+          <Route path="/blogs/:id">
+             <BlogDetails />
+          </Route>
+        </Switch>
+        
+      </div>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
---
+++ Create.js
@@ -0,0 +1,39 @@
+import { useState } from "react";
+
+const Create = () => {
+    const [title, setTitle] = useState();
+    const [body, setBody] = useState();
+    const [author, setAuthor] = useState('kafa');
+    return (
+        <div className="create">
+            <h2>Add a new Blog</h2>
+            <form>
+                <label>Blog Title:</label>
+                <input 
+                type="text" 
+                required
+                value = {title}
+                onChange = {(e) => setTitle(e.target.value)}
+                />
+                <label>Blog Body:</label>
+                <textarea 
+                required
+                value = {body}
+                onChange = {(e) => setBody(e.target.value)}
+                />
+                <label>Blog Author:</label>
+               <select
+                value = {author}
+                onChange = {(e) => setAuthor(e.target.value)}
+               >
+                   <option value="kambiz fakhr">kambiz fakhr</option>
+                   <option value="kafa">kafa</option>
+               </select>
+               <button>Insert Blog</button>
+               <p>{ author }</p>
+            </form>
+        </div>
+      );
+}
+ 
+export default Create;
